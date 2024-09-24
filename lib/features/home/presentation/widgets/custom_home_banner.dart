055883698ed06fbc2634ab7c6d597297b448e5d8
DESKTOP-OI5QP1N\Intel <osamagamil843@gmail.com>
2024-09-24 14:31:58 +0300

    home ui done

--- lib/features/home/presentation/widgets/custom_home_banner.dart
+++ lib/features/home/presentation/widgets/custom_home_banner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../core/helpers/spacer.dart';
 import '../../../../core/theme/app_pallete.dart';
 import '../../../../core/theme/font_weight_helper.dart';
 
@@ -11,70 +12,77 @@ class CustomHomeBanner extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: 348.w,
-      height: 200.h,
+      height: 117.h,
       child: Stack(
         children: [
           Image.asset(
             "assets/home_frame.png",
             width: 348.w,
-            height: 200.h,
+            height: 117.h,
             fit: BoxFit.fill,
           ),
-          Row(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "600",
+          Padding(
+            padding: EdgeInsets.only(
+                left: 15.w, bottom: 20.h, top: 10.h, right: 7.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "600",
+                          style: TextStyle(
+                              color: AppPallete.whiteColor,
+                              fontSize: 30.h,
+                              fontWeight: FontWeightHelper.bold,
+                              fontFamily: "Inter"),
+                        ),
+                        Text(
+                          "Students",
+                          style: TextStyle(
+                              color: AppPallete.whiteColor,
+                              fontSize: 12.24.h,
+                              fontWeight: FontWeightHelper.regular,
+                              fontFamily: "Inter"),
+                        )
+                      ],
+                    ),
+                    verticalSpace(5),
+                    Container(
+                      constraints: BoxConstraints(maxWidth: 158.28.w),
+                      child: Text(
+                        "in faculty of Computers and Artificial Intelligence",
                         style: TextStyle(
                             color: AppPallete.whiteColor,
-                            fontSize: 30.h,
-                            fontWeight: FontWeightHelper.bold,
+                            fontSize: 11.24.h,
+                            fontWeight: FontWeightHelper.semiBold,
                             fontFamily: "Inter"),
                       ),
-                      Text(
-                        "Students",
-                        style: TextStyle(
-                            color: AppPallete.whiteColor,
-                            fontSize: 12.24.h,
-                            fontWeight: FontWeightHelper.regular,
-                            fontFamily: "Inter"),
-                      )
-                    ],
-                  ),
-                  Container(
-                    constraints: BoxConstraints(maxWidth: 158.28.w),
+                    )
+                  ],
+                ),
+                MaterialButton(
+                    color: AppPallete.whiteColor,
+                    onPressed: () {},
+                    height: 27.49.h,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(7.0.r),
+                    ),
                     child: Text(
-                      "in faculty of Computers and Artificial Intelligence",
+                      "Show more Details",
                       style: TextStyle(
-                          color: AppPallete.whiteColor,
+                          color: const Color(0xff495ECA),
                           fontSize: 11.24.h,
                           fontWeight: FontWeightHelper.semiBold,
                           fontFamily: "Inter"),
-                    ),
-                  )
-                ],
-              ),
-              MaterialButton(
-                  color: AppPallete.whiteColor,
-                  onPressed: () {},
-                  height: 27.49.h,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(7.0.r),
-                  ),
-                  child: Text(
-                    "Show more Details",
-                    style: TextStyle(
-                        color: const Color(0xff495ECA),
-                        fontSize: 11.24.h,
-                        fontWeight: FontWeightHelper.semiBold,
-                        fontFamily: "Inter"),
-                  ))
-            ],
+                    ))
+              ],
+            ),
           )
         ],
       ),
--- lib/features/home/presentation/widgets/custom_home_catergories.dart
+++ lib/features/home/presentation/widgets/custom_home_catergories.dart
@@ -4,6 +4,7 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import '../../../../core/helpers/spacer.dart';
 import '../../../../core/theme/font_weight_helper.dart';
+import '../../domain/entity/home_categories.dart';
 
 class CustomHomeCatergories extends StatelessWidget {
   const CustomHomeCatergories({super.key});
@@ -11,62 +12,69 @@ class CustomHomeCatergories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: MasonryGridView.builder(
-          gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2),
-          itemCount: 2,
-          itemBuilder: (context, i) => Stack(
-                children: [
-                  Container(
-                    height: i == 1 ? 250.h : 208.h,
-                    width: 168.w,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.r),
-                        color: const Color(0xff70C1D4)),
-                  ),
-                  Container(
-                    height: i == 1 ? 250.h : 208.h,
-                    width: 168.w,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.r),
-                        gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              const Color(0xff000000).withOpacity(0.0),
-                              const Color(0xff000000).withOpacity(0.8)
-                            ])),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Spacer(),
-                        Image.asset(
-                          "assets/doctors_data.png",
-                          width: 113.w,
-                          height: 113.h,
-                        ),
-                        verticalSpace(4),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0).r,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Doctors' data",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeightHelper.regular,
-                                    fontFamily: "Inter",
-                                    fontSize: 16.57.h),
+      child: Padding(
+        padding: EdgeInsets.all(6.0.r),
+        child: MasonryGridView.builder(
+            gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2),
+            itemCount: homeCategories.length,
+            itemBuilder: (context, i) => Padding(
+                  padding: EdgeInsets.all(6.0.r),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: i == 0 || i == 2 ? 230.h : 180.h,
+                        width: 168.w,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.r),
+                            color: homeCategories[i].color),
+                      ),
+                      Container(
+                        height: i == 0 || i == 2 ? 230.h : 180.h,
+                        width: 168.w,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.r),
+                            gradient: LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  const Color(0xff000000).withOpacity(0.0),
+                                  const Color(0xff000000).withOpacity(0.0),
+                                  const Color(0xff000000).withOpacity(0.8)
+                                ])),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Spacer(),
+                            Image.asset(
+                              homeCategories[i].image,
+                              width: i == 0 || i == 2 ? 113.w : 87.w,
+                              height: i == 0 || i == 2 ? 113.w : 87.w,
+                            ),
+                            verticalSpace(4),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0).r,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    homeCategories[i].title,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeightHelper.regular,
+                                        fontFamily: "Inter",
+                                        fontSize: 16.57.h),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              )),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+      ),
     );
   }
 }
