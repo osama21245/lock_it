530b5fcf76759af82044e4d3dd5de9172a494318
osama ahmed <osamagamil2070@gmail.com>
2024-09-29 03:26:17 +0300

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:lock_it/core/theme/app_pallete.dart';
+import 'package:lock_it/core/theme/theme_data.dart';
 import 'package:lock_it/features/home/presentation/screens/home_screen.dart';
 
 void main() {
@@ -18,32 +18,8 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            scaffoldBackgroundColor: AppPallete.backgroundColor,
-            textTheme: const TextTheme(
-              displayLarge: TextStyle(
-                fontSize: 27,
-                color: AppPallete.titleTextColor,
-                fontWeight: FontWeight.bold,
-              ),
-              displayMedium: TextStyle(
-                  fontSize: 18,
-                  color: Color.fromARGB(154, 195, 193, 193),
-                  fontWeight: FontWeight.w400,
-                  wordSpacing: 1.2,
-                  height: 1.2),
-              displaySmall: TextStyle(
-                fontSize: 18,
-                color: AppPallete.btnBorderColor,
-                fontWeight: FontWeight.bold,
-              ),
-              headlineMedium: TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            )),
+        title: 'Lock It',
+        theme: AppTheme.appDarkTheme,
         home: const HomeScreen(),
       ),
     );
